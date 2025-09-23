5eb8b64a3ffc7b0e2a9f5a858ccf7619a7316da1
2keller <mashayamombekelvin@gmail.com>
2025-09-23 20:54:35 +0200

    added the database model to the admin panel

--- bettracker/slips/admin.py
+++ bettracker/slips/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Betslip
 
 # Register your models here.
+
+@admin.register(Betslip)
+class BetslipAdmin(admin.ModelAdmin):
+    list_display = ('slip_code', 'bookmaker', 'status', 'checked_at')
+    search_fields = ('slip_code', 'bookmaker')
+    list_filter = ('status',)
+    ordering = ('-checked_at',)
+
+    def has_add_permission(self, request, obj=None):
+        return False  # Disable adding new slips through the admin interface
